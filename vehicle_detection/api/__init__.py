4b2f833b1f10cdaa2fb5ed0d27268cc8912c0a95
wangbingsum <wangbing_sum@126.com>
2022-05-17 19:50:12 +0800

    添加对检测结果进行分析的代码

--- vehicle_detection/api/__init__.py
+++ vehicle_detection/api/__init__.py
@@ -1,2 +1,3 @@
 from .model_test import *
 from .load_image import *
+from .analyse import *
---
+++ vehicle_detection/api/analyse.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+'''
+@Description:对检测结果进行简单分析
+@Date       :2022/05/17 19:48:55
+@Author     :wangbing
+@version    :1.0
+'''
+
+import tqdm
+
+
+__all__ = ["analyse_result"]
+
+
+def analyse_result(result):
+    pass_count = 0
+    
+    for res in tqdm(result):
+        if res[2] == "OK":
+            pass_count += 1
+    print(f"pass: {pass_count / len(result) * 100 :04f}%({pass_count}/{len(result)})")
